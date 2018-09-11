9b736b4de652f3b322e6cde19d7ac38b84a10bf0
Ilya Kreymer <ikreymer@gmail.com>
2018-09-11 12:06:51 -0700

    cleanup: fix typos, remove duplicate audio_ws.js

--- shepherd/jsControllers/webpack.config.js
+++ shepherd/jsControllers/webpack.config.js
@@ -5,6 +5,6 @@ module.exports = {
   entry: './index.js',
   output: {
     path: path.resolve(__dirname, '..', 'static'),
-    filename: 'shepard.bundle.js'
+    filename: 'shepherd.bundle.js'
   }
 };
--- shepherd/jsControllers/webpack.docker.config.js
+++ shepherd/jsControllers/webpack.docker.config.js
@@ -3,6 +3,6 @@ module.exports = {
   entry: './index.js',
   output: {
     path: '/output',
-    filename: 'shepard.bundle.js'
+    filename: 'shepherd.bundle.js'
   }
 };
